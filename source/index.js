f7e68697dcd6ae8f0e88a730ef621fbf95814c46
Silvia Donelli <silvia.donelli@gmail.com>
2021-04-18 17:00:19 -0300

    Fix error: No Access-Control-Allow-Origin header is present on the requested resource

--- source/index.js
+++ source/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(cors());
 
 var users = [
   /* Data for testing */
