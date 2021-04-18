c0177aa8d597adf9278847a6214c3d1231c7d281
Silvia Donelli <silvia.donelli@gmail.com>
2021-04-18 15:30:45 -0300

    Include handleRegister

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # activity-11
+
+## How to install?
+
+ $ npm install
+ $ npm start
+
+## Running on
+
+http://localhost:4000/
--- source/index.js
+++ source/index.js
@@ -1,8 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(express.urlencoded());
+app.use(express.json());
 
-app.listen(4000)
+var users = [
+  /* Data for testing */
+  { email: "test@gmail.com", fullName: "Test App", password: "12345678" },
+];
+
+app.get("/", function (req, res) {
+  res.send("Server is up");
+});
+
+app.post("/users/register", function (req, res) {
+  handleRegister(req);
+  res.json({ code: 200, message: "User has been created." });
+});
+
+function handleRegister(registerReq) {
+  var user = {
+    email: registerReq.body.email,
+    fullName: registerReq.body.fullName,
+    password: registerReq.body.password,
+  };
+
+  users.push(user);
+}
+
+app.listen(4000);
